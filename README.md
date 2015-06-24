2fdf76bf5fc40d9f9015f7c304c29af922fdede0
3noch <eacameron@gmail.com>
2015-06-23 23:23:54 -0400

    Use stack

---
+++ README.md
@@ -0,0 +1,4 @@
+Build
+-----
+
+    $ stack setup && stack build
--- src/CovenantEyes/Api.hs
+++ src/CovenantEyes/Api.hs
@@ -2,8 +2,9 @@ module CovenantEyes.Api where
 
 import CovenantEyes.Api.Internal.Prelude
 
-import Control.Lens        ((^.))
-import Data.Aeson.Lens     (key, asText, asBool)
+import Control.Lens        ((^?))
+import qualified Data.Aeson as JSON
+import Data.Aeson.Lens     (key, _Value, _String, _Bool)
 import Data.Text.Encoding  (encodeUtf8)
 
 import CovenantEyes.Api.Internal.Endpoints
@@ -13,16 +14,16 @@ import CovenantEyes.Api.Internal.Config
 
 getApiCredsForUser :: CeApiConfig -> CeUser -> Password -> EitherT SomeException IO (ApiCredsFor CeUser)
 getApiCredsForUser config user pw = do
-  json <- downloadJson (_httpManager config) (userApiCredsRequest config user pw)
+  (json :: JSON.Value) <- downloadJson (_httpManager config) (userApiCredsRequest config user pw)
   let apiCreds = do
-        let root = Just json ^. key "result" . key "records" . key "basicAuthentication"
-        apiKey    <- encodeUtf8 <$> root ^. key "key" . asText
-        apiSecret <- encodeUtf8 <$> root ^. key "secret" . asText
+        root <- json ^? key "result" . key "records" . key "basicAuthentication" . _Value
+        apiKey    <- encodeUtf8 <$> root ^? key "key" . _String
+        apiSecret <- encodeUtf8 <$> root ^? key "secret" . _String
         return $ ApiCredsFor user (BasicAuthCreds apiKey apiSecret)
   failWith (toException UnexpectedContent) apiCreds
 
 
 getUserPanicState :: CeApiConfig -> ApiCredsFor CeUser -> EitherT SomeException IO Bool
 getUserPanicState config apiCreds = do
-  json <- downloadJson (_httpManager config) (userPanicRequest config apiCreds)
-  failWith (toException UnexpectedContent) (Just json ^. key "result" . key "records" . key "panic" . asBool)
+  (json :: JSON.Value) <- downloadJson (_httpManager config) (userPanicRequest config apiCreds)
+  failWith (toException UnexpectedContent) (json ^? key "result" . key "records" . key "panic" . _Bool)
